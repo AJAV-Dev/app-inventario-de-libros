ed6b882858a62d880b53b06106f84b4b37c0f11f
ALVAROJ <alvaro28.av@gmail.com>
2025-04-20 21:20:43 -0500

    Configuración de servidor personalizado con index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const jsonServer = require('json-server');
 const path = require('path');
 
-const server = jsonServer.create();
+const path = require('path');
 const router = jsonServer.router(path.join(__dirname, 'db.json'));
 const middlewares = jsonServer.defaults();
 
